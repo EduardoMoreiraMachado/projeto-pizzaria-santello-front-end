8510c6ba580f6485e951909734f2089757b7b188
MarinaSantello <marina.12soft@gmail.com>
2022-11-29 20:45:14 -0300

    melhorias no layout da tela de login e tentativa/teste de novas implementacoes na plataforma

---
+++ assets/css/teste-cards/main.js
@@ -0,0 +1,8 @@
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,21 +1,35 @@
-const teste = function() {
-    const email = document.getElementById('email').value
-    const senha = document.getElementById('senha').value
+// const teste = function() {
+//     const email = document.getElementById('email').value
+//     const senha = document.getElementById('senha').value
 
-    const userLoginJSON = {}
+//     const userLoginJSON = {}
 
-    userLoginJSON.email = email
-    userLoginJSON.senha = senha
+//     userLoginJSON.email = email
+//     userLoginJSON.senha = senha
 
-    fetch("http://localhost:8080/v1/loginCliente", {
-        method: "POST",
+//     fetch("http://localhost:8080/v1/loginCliente", {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json',
+//             },
+//         body: JSON.stringify(userLoginJSON)
+//     });
+
+
+// }
+
+const validarLogin = async (login) => {
+    const url = ''
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(login),
         headers: {
-            'Content-Type': 'application/json',
-            },
-        body: JSON.stringify(userLoginJSON)
-    });
+            'content-type': 'application/json',
+        },
+    };
 
+    await fetch(url, options);
+};
 
-}
-
-const button = document.getElementById('button__enter').addEventListener('click', teste)
+const button = document.getElementById('button__enter').addEventListener('click', validarLogin)
