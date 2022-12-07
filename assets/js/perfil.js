6c8c20b23707d2d42406cca6143a4b2b53e9a6e0
EddieTheBill <dudumm1234@gmail.com>
2022-12-07 11:09:13 -0300

    criação das páginas de criar usuário e editar perfil

---
+++ assets/js/perfil.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const link = window.location.search.substring(1);
+const idCliente = link.split('=')[1]
+
+const editarUsuario = async (usuario, id) => {
+    let responseJSON = {}
+
+    const url = `http://localhost:8080/v1/cliente/${id}`
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(usuario),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+
+    const response = await fetch(url, options)
+
+    responseJSON.statusCode = response.status
+    responseJSON.json = response.json()
+    
+    return responseJSON
+}
+
+const excluirUsuario = async (id) => {
+
+    const url = `http://localhost:8080/v1/cliente/${id}`
+
+    const response = await fetch(url)
+
+    const deletar = await response.json()
+
+    return deletar;
+}
+
+const editUser = async () => {
+
+    const nameUser = document.getElementById('name').value
+    const emailUser = document.getElementById('email').value
+    const passUser = document.getElementById('password').value
+    
+    var editUserJSON = {
+        nome: nameUser,
+        email: emailUser,
+        senha: passUser
+    }
+    
+    const save = await editarUsuario(editUserJSON, idCliente)
+
+    if (save.statusCode == 201) {
+        alert('AAEEEEEEE!!!!! :))))))))')
+    } else {
+        alert('AAAAAAAAAAAAAAHHHHHH!!!! ;((((((((')
+    }
+}
+
+const deleteUser = async () => {
+    
+    const excluir = await excluirUsuario(idCliente)
+
+    if (excluir.statusCode == 200) {
+        window.location.href = './welcome-page.html'
+    } else {
+        alert('AAAAAAAAAAAAAAHHHHHH!!!! ;((((((((')
+    }
+}
+
+const buttonSave = document.getElementById('save').addEventListener('click', editUser)
+const buttonDelete = document.getElementById('delete').addEventListener('click', deleteUser)
---
+++ assets/js/usuarios.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const inserirUsuario = async (usuario) => {
+    let responseJSON = {}
+
+    const url = 'http://localhost:8080/v1/cliente'
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(usuario),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }
+
+    const response = await fetch(url, options)
+
+    responseJSON.statusCode = response.status
+    responseJSON.json = response.json()
+    
+    return responseJSON
+}
+
+const saveUser = async () => {
+
+    const nameUser = document.getElementById('name').value
+    const emailUser = document.getElementById('email').value
+    const passUser = document.getElementById('password').value
+    
+    var saveUserJSON = {
+        nome: nameUser,
+        email: emailUser,
+        senha: passUser
+    }
+    
+    const save = await inserirUsuario(saveUserJSON)
+
+    if (save.statusCode == 201) {
+        alert('AAEEEEEEE!!!!! :))))))))')
+    } else {
+        alert('AAAAAAAAAAAAAAHHHHHH!!!! ;((((((((')
+    }
+}
+
+const button = document.getElementById('save').addEventListener('click', saveUser)
