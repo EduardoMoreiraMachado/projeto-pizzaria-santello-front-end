457b202affd920ea12bc7be9228bd9ad35a8c3d4
Marina <98419843+MarinaSantello@users.noreply.github.com>
2022-12-07 11:15:36 -0300

    identificando e expondo dados do usuario em todas as telas do dashboard

--- assets/js/info-person.js
+++ assets/js/info-person.js
@@ -7,8 +7,12 @@ const nome01 = url.split('=')[2]
 const primeiroNome = nome01.split('%20')[0]
 const segundoNome = nome01.split('%20')[1]
 
-const perfilLink = document.getElementById('perfil')
-perfilLink.href = `./perfil.html?id_adm=${idADM}`
+const optionsArray = ['pizzas', 'bebidas', 'servicos', 'ingredientes', 'categorias', 'mensagens', 'usuarios', 'perfil']
+
+optionsArray.forEach(element => {
+    let link = document.getElementById(`${element}`)
+    link.href = `./${element}.html?${url}`    
+})
 
 const nomeADM = document.getElementById('nome__adm')
 nomeADM.textContent = `${primeiroNome} ${segundoNome}`
