35b449f98895284100b2b41db018ecdc3212fc85
marina.12soft@gmail.com <marina.12soft@gmail.com>
2022-12-11 11:18:06 -0300

    implementando os cards com informações da api na one-page

---
+++ assets/js/pizzasDesconto.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const listarPizzasDesconto = async () => {
+
+    const url = 'http://192.168.1.111:1234/v1/pizzasDesconto'
+
+    const response = await fetch(url)
+
+    const mensagens = await response.json()
+
+    return mensagens
+
+}
+
+const discountPizzasList = async () => {
+
+    const discountList = document.getElementById('discount')
+
+    const discount = await listarPizzasDesconto()
+
+    discount.forEach(element => {
+
+        const cardDiscount = document.createElement('div')
+        cardDiscount.classList.add('discount_card')
+
+        cardDiscount.innerHTML = `
+            <div class="card_header">
+                <img src="${element.foto}" class="discount_image">
+                <span class="discount_name">${element.nome_produto}</span>
+            </div>
+            <p class="discount_ingredients">${element.ingredientes}</p>
+            <span class="original_price">R$ ${element.preco}</span>
+            <h1 class="discount_price">R$ ${element.preco_descontado}</h1>
+        `
+
+        discountList.appendChild(cardDiscount)
+
+    })
+
+}
+
+discountPizzasList()
